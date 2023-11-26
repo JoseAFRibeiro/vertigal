7ea0c271aafb0f237df4b5681dc7b7911f33465a
Jose Ribeiro <josealbertoribeiro800@gmail.com>
2023-11-26 12:03:14 +0000

    added more VAOs to deal with tex coords and normals

--- src/gl/renderer.c
+++ src/gl/renderer.c
@@ -5,17 +5,16 @@
 #include "vertigal/glstrcuts.h"
 #include "vertigal/models.h"
 
+GLint shaderProgram;
 
 uint8_t rendererSetup(void)
 {
-    GLint shaderProgram;
     VG_shader_packet packet = {0};
 
     packet.vertex = compileShader("./res/vertex.glsl", GL_VERTEX_SHADER);
     packet.fragment = compileShader("./res/fragment.glsl", GL_FRAGMENT_SHADER);
     
     shaderProgram = compileShaderProgram(packet);
-    glUseProgram(shaderProgram);
     return 0;
 }
 
@@ -30,16 +29,23 @@ void renderLoop(GLFWwindow* win)
     VG_3D_ENTITY* cube = {0};
     uint32_t VBO, VAO, EBO;
 
+    cube = loadModelFromObj("./res/cube.obj");
+    glUseProgram(shaderProgram);
+    
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //FIXME: memory access violation ao mandar vertices para o GPU    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(VG_3D_VERTEX) * cube->attribs.numVertices, cube->vertexArray, GL_STATIC_DRAW);
 
-    glGenVertexArrays(1, &VAO);
+    glGenVertexArrays(3, &VAO);
     glBindVertexArray(VAO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-
-    cube = loadModelFromObj("./res/cube.obj");
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)3);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)5);
+    glEnableVertexAttribArray(2);    
+    
 
     //TODO: Deal with models returning NULL pointers
     
