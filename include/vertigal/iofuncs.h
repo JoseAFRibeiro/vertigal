0113acdb7a0f7693a2b89e20932656fd7fc5d300
Jose Ribeiro <josealbertoribeiro800@gmail.com>
2024-03-06 18:53:09 +0000

    more verbosse variables

--- include/vertigal/iofuncs.h
+++ include/vertigal/iofuncs.h
@@ -12,6 +12,6 @@ char* readFileToBuffer(size_t* size, const char* path);
 void vg_warn(char* msg);
 void vg_log(char* msg);
 void vg_error(char*msg);
-ssize_t VG_getline(char** restrict lineptr, size_t* restrict n, file_buffer_t* restrict fb, bool* isEOF);
+ssize_t VG_getline(char** restrict lineptr, size_t* restrict lineLength, file_buffer_t* restrict fb, bool* isEOF);
 
 #endif 
