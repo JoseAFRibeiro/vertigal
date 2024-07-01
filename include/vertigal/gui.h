bf84d0f27dfa9878c7a6d9c52a327239eb20fbe2
Jose Ribeiro <josealbertoribeiro800@gmail.com>
2024-07-01 21:02:03 +0100

    header files for GUI programming

---
+++ include/vertigal/gui.h
@@ -0,0 +1,7 @@
+#ifndef E__PROGRAMAS_C_VERTIGAL_INCLUDE_VERTIGAL_GUI_H_
+#define E__PROGRAMAS_C_VERTIGAL_INCLUDE_VERTIGAL_GUI_H_
+#include <GLFW/glfw3.h>
+
+uint8_t guiInit(GLFWwindow *win);
+void guiRender(GLFWwindow *win);
+#endif
